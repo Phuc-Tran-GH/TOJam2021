751c897ac0b750bf6f7e521cd6f94da13839edb2
jflowgh <justin.flowers@gamehive.com>
2021-05-14 18:33:54 -0400

    Add healthbar logic

--- Assets/Scripts/CannonBehaviour.cs
+++ Assets/Scripts/CannonBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using DottedLine;
 
 public class CannonBehaviour : MonoBehaviour
 {
@@ -13,12 +14,15 @@ public class CannonBehaviour : MonoBehaviour
     private float momentumX = 1000;
     private float momentumY = 200;
 
+
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
-        lr.material = (Material) Resources.Load("Assets/BlackDotMaterial.mat");
+//        lr.material = (Material) Resources.Load("Assets/Images/BlackDotMaterial.mat");
+        lr.material.SetTextureScale("_MainTex", new Vector2(0.5f, 0.5f));
 
+        lr.textureMode =  LineTextureMode.Tile;
         // Set some positions
         Vector3[] positions = new Vector3[2];
         positions[0] = transform.position;
@@ -32,21 +36,22 @@ public class CannonBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Input.mousePosition;
     	FaceMouse();
+
+        healthbar healthbar = FindObjectOfType(typeof(healthbar)) as healthbar; 
+
         if (Input.GetMouseButtonDown(0) && !shot){
-            Vector3 mousePosition = Input.mousePosition;
             beaver = FindObjectOfType(typeof(Beaver)) as Beaver; 
             float finalY = Mathf.Abs(mousePosition.y) - 100;
-            //if (finalY < 0) finalY = 0;
-            
-            momentumY = (momentumX / 5) + finalY;
+            int cursec = healthbar.cursec;
+            momentumY = ((momentumX * (cursec / 100) / 5)) + finalY;
             beaver.ShootOutOfCannon(new Vector2(momentumX, momentumY));
 
 
 
             shot = true;
         }
-        //RedrawLine();
     }
 
     void FaceMouse()
@@ -61,7 +66,6 @@ public class CannonBehaviour : MonoBehaviour
 
         transform.right = direction;
         RedrawLine(mousePosition.x, mousePosition.y);
-
     }
 
     void RedrawLine(float x, float y)
@@ -79,6 +83,11 @@ public class CannonBehaviour : MonoBehaviour
         positions[1] = new Vector3(x, y, 0.0f);
 
         lr.SetPositions(positions);
+
+                float width =  lr.startWidth;
+        lr.material.mainTextureScale = new Vector2(Vector2.Distance(lr.GetPosition(0), lr.GetPosition(lr.positionCount - 1)) / lr.widthMultiplier, 1);
+
+
     }
 
 
