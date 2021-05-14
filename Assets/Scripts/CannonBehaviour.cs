2a1faa73d089e5a6122e79c63f78d79218d821ab
jflowgh <justin.flowers@gamehive.com>
2021-05-14 12:30:41 -0400

    Add cannon

---
+++ Assets/Scripts/CannonBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CannonBehaviour : MonoBehaviour
+{
+    
+    private LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.material = (Material) Resources.Load("Assets/BlackDotMaterial.mat");
+
+        // Set some positions
+        Vector3[] positions = new Vector3[2];
+        positions[0] = transform.position;
+        positions[1] = new Vector3(0.0f, -0.5f, 0.0f);
+        
+        lr.positionCount = positions.Length;
+        lr.SetPositions(positions);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	FaceMouse();
+        //RedrawLine();
+    }
+
+    void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+ 
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+
+        transform.right = -direction;
+        RedrawLine(mousePosition.x, mousePosition.y);
+
+    }
+
+    void RedrawLine(float x, float y)
+    {
+        Vector2 direction = new Vector2(
+            x - lr.transform.position.x,
+            y - lr.transform.position.y
+        );
+
+        lr.transform.right = -direction;
+
+        // Set some positions
+        Vector3[] positions = new Vector3[2];
+        positions[0] = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        positions[1] = new Vector3(x, y, 0.0f);
+
+        lr.SetPositions(positions);
+    }
+
+
+}
