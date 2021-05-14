a93cb82f8e0ba6baa1eafe3950a86aecc7bae858
Phuc Tran <phuc@gamehive.com>
2021-05-14 11:11:41 -0400

    Added tree biting

--- Assets/Scripts/Beaver.cs
+++ Assets/Scripts/Beaver.cs
@@ -4,4 +4,51 @@ using UnityEngine;
 
 public class Beaver : MonoBehaviour
 {
+	[SerializeField] private Rigidbody2D rigidbody2D;
+	[SerializeField] private GameObject biteCollider;
+
+	public float BiteDuration { get; private set; } = 0.2f;
+	
+	
+	private void Update()
+	{
+		rigidbody2D.AddForce(new Vector2(2.0f, 0)); // TODO remove this after cannon is added
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Bite();
+		}
+	}
+
+	private void Bite()
+	{
+		if (!biteCollider.activeSelf)
+		{
+			biteCollider.SetActive(true);
+			Invoke(nameof(FinishBite), BiteDuration);
+		}
+	}
+
+	private void FinishBite()
+	{
+		CancelInvoke(nameof(FinishBite));
+		biteCollider.SetActive(false);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Tree"))
+		{
+			OnTreeCollision();
+		}
+	}
+
+	private void OnTreeCollision()
+	{
+		// TODO
+	}
 }
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+	[SerializeField] private BoxCollider2D collider;
+	[SerializeField] private GameObject biteMarks;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Bite"))
+		{
+			OnBitten(other.transform.position);
+		}
+	}
+
+	private void OnBitten(Vector3 bitePosition)
+	{
+		collider.enabled = false;
+		biteMarks.transform.position = new Vector3(transform.position.x, bitePosition.y, 0);
+		biteMarks.SetActive(true);
+	}
+}
