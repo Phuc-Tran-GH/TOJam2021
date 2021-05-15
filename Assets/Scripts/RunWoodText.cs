55242ce525a32074821aaa87e3c987ca9dac31e2
jflowgh <justin.flowers@gamehive.com>
2021-05-15 14:01:51 -0400

    Add UI log elements

---
+++ Assets/Scripts/RunWoodText.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RunWoodText : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI WoodText;
+
+	private void Awake()
+	{
+		GameManager.instance.RunWoodChangedEvent += RunWoodChangedEventHandler;
+	}
+
+	private void OnDestroy()
+	{
+		GameManager.instance.RunWoodChangedEvent -= RunWoodChangedEventHandler;
+	}
+
+	private void Start()
+	{
+		UpdateText();
+	}
+
+	private void RunWoodChangedEventHandler(int wood)
+	{
+		UpdateText();
+	}
+
+	private void UpdateText()
+	{
+		WoodText.text = $"{GameManager.instance.RunWood}";
+	}
+}
