1d733a4773ad981c30fc35e8d519846de4ca5470
jflowgh <justin.flowers@gamehive.com>
2021-05-15 19:46:55 -0400

    Add high score dialog

---
+++ Assets/Scripts/LevelText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelText : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI cannonLevel;
+	[SerializeField] private TextMeshProUGUI gliderLevel;
+	[SerializeField] private TextMeshProUGUI bounceLevel;
+	private UpgradeManager upgradeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upgradeManager =  UpgradeManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Show();
+    }
+
+
+    void Show(){
+    	Debug.Log("Show!!!!");
+    	Debug.Log($"{upgradeManager.GetCannonUpgradeNum()}");
+    	cannonLevel.text = $"{upgradeManager.GetCannonUpgradeNum()}";
+    	gliderLevel.text = $"{upgradeManager.GetGliderUpgradeNum()}";
+    	bounceLevel.text = $"{upgradeManager.GetSlapUpgradeNum()}";
+    }
+
+}
